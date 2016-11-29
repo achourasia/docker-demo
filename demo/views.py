1cc4e567ea00cecf45a8b9e7eecaaeab6e3e602b
Dmitry Mishin <dmishin@sdsc.edu>
2016-11-28 18:21:17 -0800

    Redirect to subdomain works

--- demo/views.py
+++ demo/views.py
@@ -20,37 +20,36 @@ def index(request):
 
     cli = Client(base_url='unix://var/run/docker.sock')
 
-    container_id = None
+    container_name = None
 
-    if request.session.get('container_id', None):
-        container_id = request.session['container_id']
+    if request.session.get('container_name', None):
+        container_name = request.session['container_name']
 
     if(request.POST.get('action',None)):
-        if(request.POST['action'] == "start" and not container_id):
+        if(request.POST['action'] == "start" and not container_name):
             port = pick_unused_port();
             container_name = namesgenerator.get_random_name()
             container = cli.create_container(
                 image='seedme2', ports=[80],
                 name=container_name,
-                environment = {"VIRTUAL_HOST":"%s.%s"%(container_name, request.get_host)},
+                environment = {"VIRTUAL_HOST":"%s.%s"%(container_name, request.get_host())},
                 host_config=cli.create_host_config(port_bindings={
                     80: port,
                 })
             )
-            container_id = container['Id'][:7]
             request.session['container_name'] = container_name
             request.session['container_exp'] = time.time()+settings.CONTAINER_EXPIRATION
-            cli.start(container_id)
+            cli.start(container['Id'])
 
-        if(request.POST['action'] == "stop" and container_id):
+        if(request.POST['action'] == "stop" and container_name):
             try:
                 cli.remove_container(
-                    container=container_id,
+                    container=container_name,
                     force = True
                 )
             except:
                 pass
-            del request.session['container_id']
+            del request.session['container_name']
             del request.session['container_exp']
 
     return render(request, 'templates/index.html', {
--- docker/run_proxy.sh
+++ docker/run_proxy.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:80 -e DEFAULT_HOST=seedmini4.sdsc.edu:8080 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy

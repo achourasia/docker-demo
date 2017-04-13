c883270f562036a71b634b5e8d2d904a596c9ba6
Dmitry Mishin <dmitry.mishin@gmail.com>
2017-04-13 13:26:56 -0700

    Containers cleaner fixed

--- demo/management/commands/clean_containers.py
+++ demo/management/commands/clean_containers.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.sessions.models import Session
-from docker import Client
+from docker import DockerClient
 
 import time
 
@@ -11,19 +11,15 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        cli = Client(base_url='unix://var/run/docker.sock')
+        cli = DockerClient(base_url='unix://var/run/docker.sock')
         sessions = Session.objects.all()
         for session in sessions:
             data = session.get_decoded()
             if(data.get("container_name", None) and data.get("container_exp") < time.time()):
-                container_id = data["container_id"]
+                container_name = data["container_name"]
                 try:
-                    #cli.remove_container(
-                    #    container=container_id,
-                    #    force = True
-                    #)
-                    pass
+                    cli.containers.get(container_name).remove(force=True)
                 except:
                     pass
-                #session.delete()
-                self.stdout.write(self.style.SUCCESS('Successfully cleaned container %s'%container_id))
+                session.delete()
+                self.stdout.write(self.style.SUCCESS('Successfully cleaned container %s'%container_name))

8556acb84953b50811feb8376eed1967532f3271
Dmitry Mishin <dmitry.mishin@gmail.com>
2017-06-22 15:29:53 -0700

    Changed host

--- docker/run_django.sh
+++ docker/run_django.sh
@@ -1,6 +1,6 @@
 docker run -d -p 8000:8000 -v /var/run/docker.sock:/var/run/docker.sock \
 	-v /Users/dmishin/code/seedme2demo/stats:/var/stats \
-	-e "VIRTUAL_HOST=demo.seedme.org" \
+	-e "VIRTUAL_HOST=try.seedme.org" \
 	-e "VIRTUAL_PORT=8000" \
 	docker-demo
 
